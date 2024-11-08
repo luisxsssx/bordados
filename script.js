4de5e5334b914f3cb83494815ff02aa46d4593bd
luisxsssx <cd210902@gmail.com>
2024-11-07 21:35:39 -0700

    First Commit

---
+++ script.js
@@ -0,0 +1,11 @@
+  document.addEventListener("DOMContentLoaded", function () {
+    var map = L.map('map').setView([25.564030713268885, -108.45986466863316], 15);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 20,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+
+    var marker = L.marker([25.564030713268885, -108.45986466863316]).addTo(map);
+    marker.bindPopup("<b>Uniformes Y Bordados Sandoval</b><br>Centro, 81000 Guasave, Sin.").openPopup();
+});
